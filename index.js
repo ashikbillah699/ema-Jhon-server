0f6051cc6e2ed4b77fb915189b1019ed06835ff9
Md Ashik Billah <ashikbillah699@gmail.com>
2022-11-06 23:09:14 +0600

    done

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req, res)=> {
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rhwxyri.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productCollection = client.db('emajhon').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size)
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.skip(page * size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        })
+
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            console.log(ids)
+            // const objectIds = ids.map(id => ObjectId(id))
+            // const query = { _id: { $in: objectIds } };
+            const query = {};
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(port,()=> {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
